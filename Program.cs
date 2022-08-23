69eb659184d573f8a3669607ab937526e1d898db
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2022-08-23 18:32:05 +0300

    all have been done

--- Program.cs
+++ Program.cs
@@ -63,3 +63,50 @@ Holiday(4);
 Holiday(6);
 Holiday(7);
 */
+
+
+/*
+"В институте биоинформатики по офису передвигается робот. Недавно студенты из группы 
+программистов написали для него программу, по которой робот, когда заходит в комнату,
+ считает количество программистов в ней и произносит его вслух: "n программистов".
+Для того, чтобы это звучало правильно, для каждого n нужно использовать верное окончание слова.
+Напишите программу, считывающую с пользовательского ввода целое число n (неотрицательное), 
+выводящее это число в консоль вместе с правильным образом изменённым словом "программист",
+ для того, чтобы робот мог нормально общаться с людьми, например: 1 программист, 2 программиста, 5 программистов.
+В комнате может быть очень много программистов. Проверьте, что ваша программа правильно 
+обработает все случаи, как минимум до 1000 человек." */
+
+void KindOfProgrammer(int n)
+{
+    if (n == 11) Console.WriteLine($"{n} програмистов");
+    else if (n%100 == 11) Console.WriteLine($"{n} програмистов");
+
+    else if (n == 12) Console.WriteLine($"{n} програмистов");
+    else if (n == 13) Console.WriteLine($"{n} програмистов");
+    else if (n == 14) Console.WriteLine($"{n} програмистов");
+
+    else if (n%100 == 12) Console.WriteLine($"{n} програмистов");
+    else if (n%100 == 13) Console.WriteLine($"{n} програмистов");
+    else if (n%100 == 14) Console.WriteLine($"{n} програмистов");
+    
+    else if (n%10 == 1) Console.WriteLine($"{n} програмист");
+    else if (n%10 == 2) Console.WriteLine($"{n} програмиста");
+    else if (n%10 == 3) Console.WriteLine($"{n} програмиста");
+    else if (n%10 == 4) Console.WriteLine($"{n} програмиста");
+  
+    else  Console.WriteLine($"{n} програмистов");
+}
+Console.WriteLine("Please, input a number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+KindOfProgrammer(n);
+
+KindOfProgrammer(1); 
+KindOfProgrammer(11); 
+KindOfProgrammer(111); 
+KindOfProgrammer(2); 
+KindOfProgrammer(12); 
+KindOfProgrammer(22); 
+KindOfProgrammer(122); 
+KindOfProgrammer(16786); 
+KindOfProgrammer(1143432211); 
